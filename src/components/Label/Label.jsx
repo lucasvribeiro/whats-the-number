d443aad3f45dd358051ba7bf5486c57bbee085fe
lucasvribeiro <lucasvribeiro14@gmail.com>
2022-01-03 15:02:54 -0300

    Created Label Component

---
+++ src/components/Label/Label.jsx
@@ -0,0 +1,49 @@
+/**
+ * @summary This is a Label component with a two-color background gradient.
+ *          Prop-types are in the end of the file.
+ */
+
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const StyledLabel = styled.span`
+  padding: 2px 8px;
+  width: fit-content;
+  border-radius: 8px;
+  font-size: 0.8rem;
+
+  color: ${(props) => props.fnColor};
+  background: linear-gradient(
+    ${(props) => props.bgColorOne},
+    ${(props) => props.bgColorTwo}
+  );
+
+  margin: 0 4px;
+`;
+
+const Label = ({ children, fnColor, bgColorOne, bgColorTwo }) => {
+  return (
+    <StyledLabel
+      fnColor={fnColor}
+      bgColorOne={bgColorOne}
+      bgColorTwo={bgColorTwo}
+    >
+      {children}
+    </StyledLabel>
+  );
+};
+
+Label.propTypes = {
+  children: PropTypes.any,
+  fnColor: PropTypes.string,
+  bgColorOne: PropTypes.string,
+  bgColorTwo: PropTypes.string,
+};
+
+Label.defaultProps = {
+  fnColor: "#ffffff",
+  bgColorOne: "#434854",
+  bgColorTwo: "#9E9E9E",
+};
+
+export default Label;
